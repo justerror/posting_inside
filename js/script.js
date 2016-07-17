9a5db6e23caf06274278429342837f695500fecc
justerror <justerror@tut.by>
2016-07-18 00:19:34 +0300

    Основные стили двух модальных окон

--- js/script.js
+++ js/script.js
@@ -2,3 +2,19 @@ document.querySelector(".calendar__link--timing").addEventListener("click", func
   event.preventDefault();
   document.querySelector(".schedule-modal").classList.add("schedule-modal-show");
 });
+
+document.querySelector(".dropbox__btn-edit").addEventListener("click", function(event) {
+  event.preventDefault();
+  document.querySelector(".edit-modal").classList.add("edit-modal-show");
+});
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (document.querySelector(".schedule-modal").classList.contains("schedule-modal-show")) {
+      document.querySelector(".schedule-modal").classList.remove("schedule-modal-show");
+    }
+    if (document.querySelector(".edit-modal").classList.contains("edit-modal-show")) {
+      document.querySelector(".edit-modal").classList.remove("edit-modal-show");
+    }
+  }
+});
